05c433d2c0144a87f3434dae26ef1a59899ebc61
emmaajoku2 (Emmanuel Ajoku <eajoku@wave5wireless.ng>
2025-06-04 13:46:24 +0100

    Revert "Update MTNZambiaTokenRefresh.php"
    This reverts commit 9a31893fbcc21e8794f4f1c92e7f8cc1e24b4ab8.

--- app/Console/Commands/MTNZambiaTokenRefresh.php
+++ app/Console/Commands/MTNZambiaTokenRefresh.php
@@ -61,81 +61,29 @@ class MTNZambiaTokenRefresh extends Command
     private function logZambiaMTNServerDetails()
     {
         try {
-            // Get specific sending server by UID
-            $server = SendingServer::where('uid', '683e2313dd6db')->first();
+            // Get all sending servers
+            $servers = DB::table('sending_servers')->get();
             
-            if ($server) {
-                // Build query parameters based on server type
-                $parameters = [];
-                
-                // Common parameters
-                if ($server->username) {
-                    $parameters['username'] = $server->username;
-                }
-                if ($server->password) {
-                    $parameters['password'] = $server->password;
-                }
-                if ($server->api_key) {
-                    $parameters['api_key'] = $server->api_key;
-                }
-                if ($server->api_secret) {
-                    $parameters['api_secret'] = $server->api_secret;
-                }
-                
-                // Custom parameters
-                if ($server->c1) {
-                    $parameters['c1'] = $server->c1;
-                }
-                if ($server->c2) {
-                    $parameters['c2'] = $server->c2;
-                }
-                if ($server->c3) {
-                    $parameters['c3'] = $server->c3;
-                }
-                if ($server->c4) {
-                    $parameters['c4'] = $server->c4;
-                }
-                if ($server->c5) {
-                    $parameters['c5'] = $server->c5;
-                }
-                if ($server->c6) {
-                    $parameters['c6'] = $server->c6;
-                }
-                if ($server->c7) {
-                    $parameters['c7'] = $server->c7;
-                }
+            $this->info('All Sending Servers:');
+            $this->info(json_encode($servers, JSON_PRETTY_PRINT));
+            
+            Log::info('All Sending Servers:', [
+                'servers' => $servers
+            ]);
 
-                $this->info('Sending Server Details:');
-                $this->info(json_encode([
-                    'id' => $server->id,
-                    'uid' => $server->uid,
-                    'name' => $server->name,
-                    'settings' => $server->settings,
-                    'api_link' => $server->api_link,
-                    'status' => $server->status,
-                    'type' => $server->type,
-                    'query_parameters' => $parameters,
-                    'quota_value' => $server->quota_value,
-                    'quota_base' => $server->quota_base,
-                    'quota_unit' => $server->quota_unit,
-                ], JSON_PRETTY_PRINT));
+            // Also get the specific ZAMBIAMTN server if it exists
+            $zambiaServer = $servers->where('name', 'ZAMBIAMTN')->first();
+            
+            if ($zambiaServer) {
+                $this->info('ZAMBIAMTN Server Details:');
+                $this->info(json_encode($zambiaServer, JSON_PRETTY_PRINT));
                 
-                Log::info('Sending Server Details:', [
-                    'id' => $server->id,
-                    'uid' => $server->uid,
-                    'name' => $server->name,
-                    'settings' => $server->settings,
-                    'api_link' => $server->api_link,
-                    'status' => $server->status,
-                    'type' => $server->type,
-                    'query_parameters' => $parameters,
-                    'quota_value' => $server->quota_value,
-                    'quota_base' => $server->quota_base,
-                    'quota_unit' => $server->quota_unit,
+                Log::info('ZAMBIAMTN Server Details:', [
+                    'server' => $zambiaServer
                 ]);
             } else {
-                $this->warn('Sending server with UID 683e2313dd6db not found');
-                Log::warning('Sending server with UID 683e2313dd6db not found');
+                $this->warn('ZAMBIAMTN sending server not found');
+                Log::warning('ZAMBIAMTN sending server not found');
             }
         } catch (\Exception $e) {
             $this->error('Error logging sending server details: ' . $e->getMessage());
