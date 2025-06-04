65cc14d88382461c01e09229609c3547eaa7c1f1
emmaajoku2 (Emmanuel Ajoku <eajoku@wave5wireless.ng>
2025-06-04 14:47:37 +0100

    Update MTNZambiaTokenRefresh.php

--- app/Console/Commands/MTNZambiaTokenRefresh.php
+++ app/Console/Commands/MTNZambiaTokenRefresh.php
@@ -62,7 +62,7 @@ class MTNZambiaTokenRefresh extends Command
     {
         try {
             // Get all sending servers
-            $sending_servers = SendingServer::where('settings', 'MTNZAMBIA')->where('status', 1)->get();
+            $sending_servers = SendingServer::where('status', 1)->get();
             
             $this->info('All Sending Servers:');
             $this->info(json_encode($servers, JSON_PRETTY_PRINT));
